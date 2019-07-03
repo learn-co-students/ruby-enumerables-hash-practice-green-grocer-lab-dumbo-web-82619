cd7c88767e4525035422a0b18a9f773ecd159101
Jonathan Koss <jonathan_koss@yahoo.com>
2019-07-03 04:00:21 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,12 @@
 def consolidate_cart(cart)
   # code here
+  #first figure out how to convert an array into a hash 
+  #next figure out how to consolidate the items
+  #add count key and value into the hash
+  hashed_cart = cart.reduce() do |memo, (key, value)|
+    memo[key] = value
+    memo
+  end
 end
 
 def apply_coupons(cart, coupons)
