3ce7d2728536ee88aaff950467e6a4229a315180
Jonathan Koss <jonathan_koss@yahoo.com>
2019-07-04 23:43:10 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,22 +1,44 @@
-def consolidate_cart(cart)
+ def consolidate_cart(cart)
   # code here
   #first figure out how to convert an array into a hash 
   #next figure out how to consolidate the items
   #add count key and value into the hash
-   organized_cart = {}
-  count = 0
-  cart.each do |element|
-    element.each do |fruit, hash|
-      organized_cart[fruit] ||= hash
-      organized_cart[fruit][:count] ||= 0
-      organized_cart[fruit][:count] += 1
-    end
-  end
-  organized_cart
+    #Use the count enumerable
+  new_hash = {}
+  cart.each do |item|
+    if new_hash[item.keys[0]]
+      new_hash[item.keys[0]][:count] += 1
+    else
+      new_hash[item.keys[0]] = {
+        count: 1,
+        price: item.values[0][:price],
+        clearance: item.values[0][:clearance]
+      }
+    end 
+  end 
+  new_hash
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  coupons.each do |ind_coup|
+    if cart.keys.include? ind_coup[:item]
+      if cart[ind_coup[:item]][:count] >= ind_coup[:num]
+        added_name = "#{ind_coup[:item]} W/COUPON"
+        if cart[added_name]
+          cart[added_name][:count] += ind_coup[:num]
+        else
+          cart[added_name] = {
+            count: ind_coup[:num],
+            price: ind_coup[:cost]/ind_coup[:num],
+            clearance: cart[ind_coup[:item]][:clearance]
+          }
+        end
+        cart[ind_coup[:item]][:count] -= ind_coup[:num]
+      end
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
