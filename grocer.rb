e86a910f8f76165daa5fb7fabb0233411d374f0c
Jonathan Koss <jonathan_koss@yahoo.com>
2019-08-03 02:16:51 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,1 +1,28 @@
+def consolidate_cart(cart)
+  # code here  
+  new_hash = {}
+  
+  cart.each do |product|
+    product.each do |name, price|
+      if !new_hash[name]
+        new_hash[name] = price.merge(:count => 1)
+      end 
+    end 
+  end 
+end
+
+def apply_coupons(cart, coupons)
+  # code here
+  
+end
+
+def apply_clearance(cart)
+  # code here
+  
+end
+
+def checkout(cart, coupons)
+  # code here 
+ 
+end
 
