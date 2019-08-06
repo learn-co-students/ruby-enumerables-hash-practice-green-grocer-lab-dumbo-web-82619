f6e0babf0e84096e5a92ed18c0d975a338d85059
Jonathan Koss <jonathan_koss@yahoo.com>
2019-08-06 04:33:42 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -23,8 +23,13 @@ def apply_coupons(cart, coupons)
         if !!cart[new_name] 
           cart[new_name][:count] += coupon[:num]
         else 
-          cart[new_name]
+          cart[new_name] = {
+            count: coupon[:num],
+            price: coupon[:cost]/coupon[:num],
+            clearance: cart[coupon[:item]][:clearance]
+          }
         end 
+        cart[coupon[:item]][:count] -= coupon[:num]
       end 
     end 
   end 
