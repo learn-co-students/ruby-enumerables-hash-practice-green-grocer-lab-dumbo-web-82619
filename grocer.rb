1664113f99007c44d90182fad8f7c3115b7ae4c8
Jonathan Koss <jonathan_koss@yahoo.com>
2019-08-08 01:10:08 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -38,11 +38,28 @@ end
 
 def apply_clearance(cart)
   # code here
-   
+  cart.each do |item, item_info|
+    if item_info[:clearance] == true 
+      item_info[:price] = (item_info[:price] * 0.8).round(2)
+    end 
+  end 
+  cart 
 end
 
 def checkout(cart, coupons)
   # code here 
- 
+  consolidated_cart = consolidate_cart(cart)
+  applied_coupons_cart = apply_coupons(consolidated_cart, coupons)
+  applied_clearance_cart = apply_clearance(applied_coupons_cart)
+  
+  price = 0 
+  applied_clearance_cart.each do |item_name, item_info|
+    price += item_info[:price] * item_info[:count]      
+  end 
+  if price > 100
+    (price * 0.9).round(2)
+  else 
+    price  
+  end 
 end
 
